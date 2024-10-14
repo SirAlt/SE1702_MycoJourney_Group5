6b38af57da95cd6f86b1f5cd2e4d1eb7663d9094
SirAlt <69218555+SirAlt@users.noreply.github.com>
2024-10-14 23:02:48 +0700

    Add main menu (#6)
    Co-authored-by: tuanloc-1708 <97837536+tuanloc-1708@users.noreply.github.com>

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("Demo");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
